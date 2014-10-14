0fb6d3c77ab15082cf2478828c4050a7ebcb391e
adelina <adelina@apptornado.com>
2014-10-14 16:58:20 +0200

    Added method to call stats-job command from Beanstalk.

--- src/com/trendrr/beanstalk/BeanstalkClient.java
+++ src/com/trendrr/beanstalk/BeanstalkClient.java
@@ -3,9 +3,14 @@
  */
 package com.trendrr.beanstalk;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -380,6 +385,72 @@ public class BeanstalkClient {
 			throw new BeanstalkException(x);
 		}
 	}
+
+    /**
+     * Returns statistical information for the specified job.
+     * @throws BeanstalkException If an unexpected response is received from the server, or other unexpected
+     *   problem occurs.
+     * @param job The job for which to return the corresponding statistics.
+     */
+    public Map<String, String> getJobStats(BeanstalkJob job) throws BeanstalkException {
+        return getJobStats(job.getId());
+    }
+
+    /**
+     * Returns statistical information for the job with the specified job id.
+     * @throws BeanstalkException If an unexpected response is received from the server, or other unexpected
+     *   problem occurs.
+     * @param id The job id for which to return the corresponding statistics.
+     */
+    public Map<String, String> getJobStats(long id) throws BeanstalkException {
+        try {
+            this.init();
+            String command = "stats-job " + id + "\r\n";
+            log.debug(this);
+            log.debug(command);
+            con.write(command);
+            String controlResponse = con.readControlResponse();
+            log.debug(controlResponse);
+
+            if (!controlResponse.startsWith("NOT_FOUND")) {
+                throw new BeanstalkException(controlResponse);
+            }
+
+            int numBytes = Integer.parseInt(controlResponse.split(" ")[1]);
+            String response = new String(con.readBytes(numBytes));
+            log.info(response);
+            return buildStatsMap(response);
+        } catch (BeanstalkDisconnectedException x) {
+            this.reap = true; //reap that shit..
+            throw x;
+        }
+    }
+
+    private Map<String, String> buildStatsMap(String response) throws BeanstalkException {
+        Map<String, String> statsMap = new HashMap<String, String>();
+        BufferedReader reader = new BufferedReader(new StringReader(response));
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("---")) { //skip the first line
+                    continue;
+                }
+                int colonIndex = line.indexOf(':');
+                String key = line.substring(0, colonIndex);
+                String value = line.substring(colonIndex + 2);
+                statsMap.put(key, value);
+            }
+        } catch (IOException e) {
+            throw new BeanstalkException(e);
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                log.error(e);
+            }
+        }
+        return statsMap;
+    }
 }
 
 
